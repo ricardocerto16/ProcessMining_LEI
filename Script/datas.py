c1cee3d93af805a6dd140a7c4555ffea81b3ba16
Ricardo Certo <ricardocerto16@users.noreply.github.com>
2018-05-09 15:14:35 +0100

    Update datas.py

--- Script/datas.py
+++ Script/datas.py
@@ -1,12 +1,17 @@
 from datetime import datetime, timedelta
 
-now = datetime.now()
+# pip install datetime_truncate
+# truncate(datetime(2012, 2, 4, 12, 24, 50), 'minute')
+now = datetime(2012, 2, 4, 12, 24)
 
+#now = datetime.now()
 print("Tupla de Datetime: " + str(now))
 
 td = timedelta(minutes=24)
 print("Time delta: " + str(td) + "\n")
 
+
 print("Hora Atual " + str(now))
+
 print("+24 horas " + str(now + td))
 print("-24 horas " + str(now - td))
